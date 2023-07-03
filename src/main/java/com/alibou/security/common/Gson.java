138027ed72d67788979d29a028be543a0c2bf67e
thaiminhtuan135 <tuana11ht@gmail.com>
2023-07-03 20:45:31 +0700

    add api kieu thanh vien

--- src/main/java/com/alibou/security/common/Gson.java
+++ src/main/java/com/alibou/security/common/Gson.java
@@ -4,14 +4,23 @@ import com.google.gson.*;
 
 import java.lang.reflect.Type;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 public class Gson {
     public static com.google.gson.Gson gson() {
-        return new GsonBuilder().registerTypeAdapter(LocalDate.class, new JsonDeserializer<>() {
-            @Override
-            public Object deserialize(JsonElement json, Type type, JsonDeserializationContext jsonDeserializationContext) throws JsonParseException {
-                return LocalDate.parse(json.getAsJsonPrimitive().getAsString());
-            }
-        }).create();
+        return new GsonBuilder()
+                .registerTypeAdapter(LocalDate.class, new JsonDeserializer<>() {
+                    @Override
+                    public Object deserialize(JsonElement json, Type type, JsonDeserializationContext jsonDeserializationContext) throws JsonParseException {
+                        return LocalDate.parse(json.getAsJsonPrimitive().getAsString());
+                    }
+                })
+                .registerTypeAdapter(LocalDateTime.class, new JsonDeserializer<LocalDateTime>() {
+                    @Override
+                    public LocalDateTime deserialize(JsonElement json, Type type, JsonDeserializationContext context) throws JsonParseException {
+                        return LocalDateTime.parse(json.getAsJsonPrimitive().getAsString());
+                    }
+                })
+                .create();
     }
 }
--- src/main/java/com/alibou/security/controller/admin/KieuThanhVienController.java
+++ src/main/java/com/alibou/security/controller/admin/KieuThanhVienController.java
@@ -2,16 +2,21 @@ package com.alibou.security.controller.admin;
 
 import com.alibou.security.Entity.KieuThanhVien;
 import com.alibou.security.Entity.User;
+import com.alibou.security.common.Gson;
+import com.alibou.security.common.Helper;
 import com.alibou.security.service.kieuThanhVien.KieuThanhVienService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @CrossOrigin
 @RestController
@@ -20,6 +25,7 @@ import java.util.List;
 public class KieuThanhVienController {
     @Autowired
     private KieuThanhVienService kieuThanhVienService;
+    private final com.google.gson.Gson gson = Gson.gson();
 
     @GetMapping
     public Page<KieuThanhVien> pagination(@RequestParam(defaultValue = "0") int pageNo,
@@ -30,4 +36,34 @@ public class KieuThanhVienController {
         return new PageImpl<>(list, pageable, list.size());
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getById(@PathVariable Integer id) {
+        try {
+            KieuThanhVien kieuThanhVien = kieuThanhVienService.getById(id).get();
+            return new ResponseEntity<>(kieuThanhVien, HttpStatus.OK);
+        } catch (NoSuchElementException e) {
+            return new ResponseEntity<>("Không tìm thấy kiểu thành viên", HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PutMapping("/{id}/edit")
+    public ResponseEntity<?> update(@PathVariable Integer id, @RequestBody String dataUpdate) {
+//        Gson gson = new Gson();
+        return kieuThanhVienService.getById(id).map(kieuThanhVien -> {
+            KieuThanhVien kieuThanhVien1 = gson.fromJson(dataUpdate, KieuThanhVien.class);
+            kieuThanhVien1.setId(kieuThanhVien.getId());
+            return new ResponseEntity<>(kieuThanhVienService.save(kieuThanhVien1), HttpStatus.OK);
+        }).orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable Integer id) {
+        try {
+            KieuThanhVien kieuThanhVien = kieuThanhVienService.getById(id).get();
+            kieuThanhVienService.delete(id);
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (NoSuchElementException e) {
+            return new ResponseEntity<>("Không tìm thấy kiểu thành viên", HttpStatus.NOT_FOUND);
+        }
+    }
 }
